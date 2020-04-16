eeb276165e0d677d097e62b8a9d347bba5b1a393
bekkkdd <bekdauletushkempirov@gmail.com>
2020-04-16 19:57:40 +0600

    Added Car model, and hello model 10 times

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car(1, 10000 , "BMW x5");
         for (int i = 0; i < 10; i++) {
-            System.out.println("Privet mir!");
+            System.out.println("Privet mir!" + car.getModel());
         }
     }
 }
