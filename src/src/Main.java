6fb6768b4b7b87035b407978ffb2d0548d9e5c04
bekkkdd <bekdauletushkempirov@gmail.com>
2020-04-16 19:47:34 +0600

    Added User model, and hello him 10 times

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        User user = new User(18, "Bekdaulet");
         for (int i = 0; i < 10; i++) {
-            System.out.println("Privet mir!");
+            System.out.println("Privet, "  + user.getName());
         }
     }
 }
